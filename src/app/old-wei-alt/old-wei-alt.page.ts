931e3eab4c81f0b10e3595d17332b40fe5c4d829
jair-chaguay <jchaguay@espol.edu.ec>
2024-08-19 14:10:48 -0500

    update

--- src/app/old-wei-alt/old-wei-alt.page.ts
+++ src/app/old-wei-alt/old-wei-alt.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonRange} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-old-wei-alt',
   templateUrl: './old-wei-alt.page.html',
   styleUrls: ['./old-wei-alt.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonRange]
 })
 export class OldWeiAltPage implements OnInit {
 
