5c8c76bb24419fa829bfd587014ecfaf469f8e02
jair-chaguay <jchaguay@espol.edu.ec>
2024-08-29 06:39:44 -0500

    Update Years, height and weight

--- src/app/old-wei-alt/old-wei-alt.page.ts
+++ src/app/old-wei-alt/old-wei-alt.page.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonRange} from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonButton, IonTitle, IonToolbar,IonItem, IonRange, IonButtons, IonProgressBar,IonLabel, IonSegment,
+  IonSegmentButton,IonBackButton} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-old-wei-alt',
   templateUrl: './old-wei-alt.page.html',
   styleUrls: ['./old-wei-alt.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonRange]
+  imports: [IonContent, IonProgressBar, IonHeader, IonButton,IonItem, IonTitle, IonToolbar, CommonModule, FormsModule, IonRange, 
+    IonButtons,IonBackButton, IonSegmentButton,IonLabel, IonSegment,]
 })
 export class OldWeiAltPage implements OnInit {
 
+  peso: number= 82;
+  altura: number= 185;
+edad: number = 40;
   constructor() { }
 
   ngOnInit() {
