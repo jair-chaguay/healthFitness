339b52517fd44babf8a73171584837e01ba056a4
Moises Alvarez <mfalvare@espol.edu.ec>
2024-08-31 15:53:01 -0500

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,33 +1,33 @@
-import { Routes } from '@angular/router';
 
+import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'inicio', // Redirige a 'inicio' por defecto
-    pathMatch: 'full'
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
   {
-    path: 'inicio',
-    loadComponent: () => import('./inicio/inicio.page').then(m => m.InicioPage)
-  },
-  {
-    path: 'tabs',
+    path:'tabs',
     loadComponent: () => import('./tabs/tabs.page').then(m => m.TabsPage)
   },
   {
     path: 'login',
-    loadComponent: () => import('./login/login.page').then(m => m.LoginPage)
+    loadComponent: () => import('./login/login.page').then( m => m.LoginPage)
   },
   {
     path: 'signup',
-    loadComponent: () => import('./signup/signup.page').then(m => m.SignupPage)
+    loadComponent: () => import('./signup/signup.page').then( m => m.SignupPage)
+  },
+  {
+    path: 'inicio',
+    loadComponent: () => import('./inicio/inicio.page').then( m => m.InicioPage)
   },
   {
     path: 'gender',
-    loadComponent: () => import('./gender/gender.page').then(m => m.GenderPage)
+    loadComponent: () => import('./gender/gender.page').then( m => m.GenderPage)
   },
   {
     path: 'old-wei-alt',
-    loadComponent: () => import('./old-wei-alt/old-wei-alt.page').then(m => m.OldWeiAltPage)
+    loadComponent: () => import('./old-wei-alt/old-wei-alt.page').then( m => m.OldWeiAltPage)
   },
+  
 ];
