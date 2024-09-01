447f5d9cb6cbf574673385d80ea22e948f817fad
crissvinces <cvinces@espol.edu.ec>
2024-09-01 17:50:20 -0500

    tab 3

---
+++ src/app/models/recipe.model.ts
@@ -0,0 +1,6 @@
+export interface Recipe {
+    title: string;
+    calories: number;
+    image: string;
+  }
+  
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  edamamAppId: 'fd1c19ff',
+  edamamApiKey: 'c5c0677324d0deadffe8f4477ec1ec36'
 };
